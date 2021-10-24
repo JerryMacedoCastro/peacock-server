4a0977a97f623fb3396c26c994e6540869c4849c
Jerry Macedo Castro <leom551@hotmail.com>
2021-10-24 16:14:25 -0300

    configure eslint/prettier

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-
-import express from 'express';
+import express from "express";
 
 const app = express();
 
 app.use(express.json());
 
-
 app.listen(3333, async () => {
-  console.log('🚀 Server started on port 3333!');
+  console.log("🚀 Server started on port 3333!");
 });
 
+function test(a: number, b: number) {
+  return a + b;
+}
