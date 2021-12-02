8b34ed2caaa9019a6a5ebac98c4936464164e5d4
Jerry Macedo Castro <leom551@hotmail.com>
2021-12-02 19:38:33 -0300

    adjust sructure

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,40 @@
-import * as bodyParser from 'body-parser';
+import * as bodyParser from "body-parser";
 // import cookieParser from 'cookie-parser';
-import express from 'express';
-import Controller from "../interface/controller.interface"
-import errorMiddleware from '../middleware/error.middleware';
+import express from "express";
+import Controller from "./interfaces/controller.interface";
+import errorMiddleware from "./middleware/error.middleware";
 
 class App {
-    public app: express.Application;
+  public app: express.Application;
 
-    constructor(controllers: Controller[]) {
-        this.app = express();
+  constructor(controllers: Controller[]) {
+    this.app = express();
 
-        this.initializeMiddlewares();
-        this.initializeErrorHandling();
-        this.initializeControllers(controllers);
-    }
+    this.initializeMiddlewares();
+    this.initializeErrorHandling();
+    this.initializeControllers(controllers);
+  }
 
-    public listen(): void {
-        this.app.listen(process.env.PORT || 3333, () => {
-            console.log(`App listening on the port ${(process.env.PORT || 3333)}`);
-        });
-    }
+  public listen(): void {
+    this.app.listen(process.env.PORT || 3333, () => {
+      console.log(`App listening on the port ${process.env.PORT || 3333}`);
+    });
+  }
 
-    private initializeMiddlewares() {
-        this.app.use(express.json());
-        // this.app.use(cookieParser());
-    }
+  private initializeMiddlewares() {
+    this.app.use(express.json());
+    // this.app.use(cookieParser());
+  }
 
-    private initializeErrorHandling() {
-        this.app.use(errorMiddleware);
-    }
+  private initializeErrorHandling() {
+    this.app.use(errorMiddleware);
+  }
 
-    private initializeControllers(controllers: Controller[]) {
-        controllers.forEach(controller => {
-            this.app.use('/', controller.router);
-        });
-    }
+  private initializeControllers(controllers: Controller[]) {
+    controllers.forEach((controller) => {
+      this.app.use("/", controller.router);
+    });
+  }
 }
 
 export default App;
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
 import express from "express";
-import { createConnection } from 'typeorm';
+import { createConnection } from "typeorm";
 import config from "../ormconfig";
 import App from "./app";
 
-import { TeacherController } from "../orm/teacher/teacher.controller";
-import { ClassController } from "../orm/class/class.controller";
-import { RoadMapController } from "../orm/roadMap/roadMap.controller";
+import { TeacherController } from "./repositories/teacher/teacher.controller";
+import { ClassController } from "./repositories/class/class.controller";
+import { RoadMapController } from "./repositories/roadMap/roadMap.controller";
 
 (async () => {
   try {
     await createConnection(config);
-
   } catch (error) {
-    console.log('Error while connecting to the database');
-    console.log(error)
+    console.log("Error while connecting to the database");
+    console.log(error);
   }
   const app = new App([
     new TeacherController(),
     new ClassController(),
-    new RoadMapController()
+    new RoadMapController(),
   ]);
 
   app.listen();
